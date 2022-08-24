215898b92fbe8b48c49133fa32db842ede0a093f
Nifori <benjamin.duesterhues@gmail.com>
2022-08-24 23:49:48 +0200

    remove sout

--- service/src/main/java/nifori/me/nifobot/service/NBServiceMain.java
+++ service/src/main/java/nifori/me/nifobot/service/NBServiceMain.java
@@ -2,8 +2,6 @@ package nifori.me.nifobot.service;
 
 import java.util.List;
 
-import nifori.me.nifobot.feature.NBFeature;
-import nifori.me.nifobot.ports.PortObservationUpdater;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -12,13 +10,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.togglz.core.manager.FeatureManager;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import lombok.extern.log4j.Log4j2;
 import nifori.me.nifobot.EventHandler.AbstractEventHandler;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.togglz.core.manager.FeatureManager;
+import nifori.me.nifobot.ports.PortObservationUpdater;
 
 @SpringBootApplication(scanBasePackages = {"nifori.me"})
 @EnableJpaRepositories(basePackages = "nifori.me.persistence.repository")
@@ -44,7 +43,6 @@ public class NBServiceMain {
   @Bean
   public CommandLineRunner run(ApplicationContext ctx) {
     return (args) -> {
-      System.out.println(featureManager.isActive(NBFeature.PORT_OBSERVATION));
       final DiscordClient client = DiscordClient.create(id);
       final GatewayDiscordClient gateway = client.login()
           .block();
